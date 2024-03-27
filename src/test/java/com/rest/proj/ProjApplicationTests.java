588ea85d3d4b305992d3a01293f6be7d5e583b83
sik2 <bickwang123@gmail.com>
2024-03-27 14:31:57 +0900

    init

---
+++ src/test/java/com/rest/proj/ProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
