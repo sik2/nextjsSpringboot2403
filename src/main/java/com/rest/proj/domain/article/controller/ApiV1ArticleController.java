c2a76bffac57d1d6073bbaf77fdb0dc5076585a7
sik2 <bickwang123@gmail.com>
2024-03-28 16:39:10 +0900

    Response obj

--- src/main/java/com/rest/proj/domain/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/rest/proj/domain/article/controller/ApiV1ArticleController.java
@@ -3,6 +3,8 @@ package com.rest.proj.domain.article.controller;
 import com.rest.proj.domain.article.entity.Article;
 import com.rest.proj.domain.article.service.ArticleService;
 import com.rest.proj.global.RsData.RsData;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,19 +16,32 @@ import java.util.List;
 @RequestMapping("/api/v1/articles")
 public class ApiV1ArticleController {
     private final ArticleService articleService;
+
+    @AllArgsConstructor
+    @Getter
+    public static class ArticlesResponse {
+        private final List<Article> articles;
+    }
+
     @GetMapping("")
-    public RsData<List<Article>> getArticles() {
+    public RsData<ArticlesResponse> getArticles() {
         List<Article> articles = this.articleService.getList();
 
-        return RsData.of("S-1", "성공", articles);
+        return RsData.of("S-1", "성공", new ArticlesResponse(articles));
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public  static class ArticleResponse {
+        private final Article article;
     }
 
     @GetMapping("/{id}")
-    public RsData<Article> getArticle (@PathVariable("id") Long id) {
+    public RsData<ArticleResponse> getArticle (@PathVariable("id") Long id) {
         return articleService.getArticle(id).map(article -> RsData.of(
                     "S-1",
                     "성공",
-                    article
+                    new ArticleResponse(article)
             )).orElseGet(() -> RsData.of(
                     "F-1",
                     "%d 번 게시물은 존재하지 않습니다.".formatted(id),
