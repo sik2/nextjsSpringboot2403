dee9bf6b09f6f0fa18c0e36ab3f0824f66d91ffe
sik2 <bickwang123@gmail.com>
2024-03-29 16:43:51 +0900

    sb cors

--- frontapp/src/app/article/page.tsx
+++ frontapp/src/app/article/page.tsx
@@ -1,10 +1,13 @@
-import Link from "next/link";
+'use client'
+export default async function Article () {
 
-export default function Article () {
+    await fetch("http://localhost:8090/api/v1/articles");
+        
+
     return (
         <>  
             <div>
-                게시판
+
             </div>
         </>
     )
