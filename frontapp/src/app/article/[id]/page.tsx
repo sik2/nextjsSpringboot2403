3150b8e41c4de7309b557b51b9dd4ec7b629d63e
sik2 <bickwang123@gmail.com>
2024-04-04 15:15:52 +0900

    front article detail

--- frontapp/src/app/article/[id]/page.tsx
+++ frontapp/src/app/article/[id]/page.tsx
@@ -1,12 +1,26 @@
 'use client'
 
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function ArticleDetail() {
-    const params = useParams();    
+    const params = useParams();  
+    const [article, setArticle] = useState({});
+
+
+    useEffect(() => {
+        fetch(`http://localhost:8090/api/v1/articles/${params.id}`)
+        .then(result => result.json())
+        .then(result => setArticle(result.data.article))
+    }, [])
+
     return ( 
         <>
             <h1>게시판 상세 {params.id}번</h1>
+            <div>{article.subject}</div>
+            <div>{article.content}</div>
+            <div>{article.createdDate}</div>
+            <div>{article.modifiedDate}</div>
         </>
     );
 }
--- frontapp/src/app/article/page.tsx
+++ frontapp/src/app/article/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import Link from "next/link";
 import { useEffect, useState } from "react"
 
 export default function Article () {
+
+
     const [articles, setArticles] = useState([]);
 
     useEffect(() => {
@@ -15,7 +18,7 @@ export default function Article () {
         <>  
            <ul>
                 번호 / 제목 / 생성일
-                {articles.map(row => <li>{row.id} / {row.subject} / {row.createdDate}</li>)}
+                {articles.map(row => <li key={row.id}>{row.id} / <Link href={`/article/${row.id}`}>{row.subject}</Link> / {row.createdDate}</li>)}
            </ul>
         </>
     )
