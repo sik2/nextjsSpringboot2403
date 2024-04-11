230d67c340d7405c4497bc85d95709a47552a1ba
sik2 <bickwang123@gmail.com>
2024-04-11 16:19:52 +0900

    access token cookie

--- src/main/java/com/rest/proj/domain/member/controller/ApiV1memberController.java
+++ src/main/java/com/rest/proj/domain/member/controller/ApiV1memberController.java
@@ -4,11 +4,14 @@ import com.rest.proj.domain.member.dto.MemberDto;
 import com.rest.proj.domain.member.entity.Member;
 import com.rest.proj.domain.member.service.MemberService;
 import com.rest.proj.global.RsData.RsData;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -33,10 +36,21 @@ public class ApiV1memberController {
     }
 
     @PostMapping("/login")
-    public RsData<LoginResponseBody> login (@Valid @RequestBody LoginRequestBody loginRequestBody) {
+    public RsData<LoginResponseBody> login (@Valid @RequestBody LoginRequestBody loginRequestBody, HttpServletResponse resp) {
 
         // username, password => accessToken
         RsData<MemberService.AuthAndMakeTokensResponseBody> authAndMakeTokensRs = memberService.authAndMakeTokens(loginRequestBody.getUsername(), loginRequestBody.getPassword());
+
+
+        ResponseCookie cookie = ResponseCookie.from("accessToken", authAndMakeTokensRs.getData().getAccessToken())
+                        .path("/")
+                        .sameSite("None")
+                        .secure(true)
+                        .httpOnly(true)
+                        .build();
+
+        resp.addHeader("Set-Cookie", cookie.toString());
+
 
         return RsData.of(authAndMakeTokensRs.getResultCode(),authAndMakeTokensRs.getMsg(), new LoginResponseBody(new MemberDto(authAndMakeTokensRs.getData().getMember())));
     }
